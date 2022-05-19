4be241446c8a2645c46a52617a36dc4d4ec4d840
Aqil Zeeshan <azeeshan@freewheel.com>
2022-05-19 12:02:23 +0100

    Remove Slider

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,3 +1,5 @@
+import { Splide,SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css"
 import { useEffect, useState } from "react";
 import styled from "styled-components"
 
@@ -11,7 +13,7 @@ function Popular() {
   },[])
 
   const getPopular = async () => {
-    const api = await fetch(`https://3j98w6n9gf.execute-api.us-east-1.amazonaws.com/v0/recipes`)
+    const api = await fetch(`https://kovw0n5o44.execute-api.us-east-1.amazonaws.com/v0/recipes`)
     const data = await api.json();
     console.log(data)
     setPopular(data.recipes)
@@ -20,18 +22,36 @@ function Popular() {
   return (
     <div>{popular.map(recipe=>{
       return(
-        <div key={recipe.id}>
-          <p>{recipe.title}</p>
-        </div>
+        <Wrapper>
+          <h3>Popular Picks</h3>
+            <Splide>
+              {
+                popular.map((recipe)=>{
+                  return(
+                    <SplideSlide>
+                      <Card>
+                        <p>{recipe.title}</p>
+                        <img src={recipe.image} alt={recipe.title} />
+                      </Card>
+                    </SplideSlide>
+                  )
+                })
+              }
+            </Splide>
+        </Wrapper>
       )
     })}</div>
   )
 
 }
 
-const Wrapper = styled.dev`
+const Wrapper = styled.div`
   margin: 4rem 0rem;
 `
 
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+`
 
 export default Popular
